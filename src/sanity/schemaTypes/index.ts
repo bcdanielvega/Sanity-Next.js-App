e20122ce01e09aa57ed94ce5a495c0919645e364
danielalexvega <danielalexvega@gmail.com>
2024-08-13 13:10:59 -0500

    add artistTyoe

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -5,7 +5,8 @@ import {categoryType} from './categoryType'
 import {postType} from './postType'
 import {authorType} from './authorType'
 import {albumReviewType} from './albumReviewType'
+import { artistType } from './artistType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, authorType, albumReviewType],
+  types: [blockContentType, categoryType, postType, authorType, albumReviewType, artistType],
 }
