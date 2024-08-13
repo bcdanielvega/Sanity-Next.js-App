a978a328d838aa1ca51337b9f8d3d5d61a4df010
danielalexvega <danielalexvega@gmail.com>
2024-08-12 20:47:40 -0500

    add albumReviewType

---
+++ src/sanity/schemaTypes/albumReviewType.ts
@@ -0,0 +1,81 @@
+import {DocumentTextIcon} from '@sanity/icons'
+import {defineArrayMember, defineField, defineType} from 'sanity'
+
+export const albumReviewType = defineType({
+  name: 'albumReview',
+  title: 'Album Review',
+  type: 'document',
+  icon: DocumentTextIcon,
+  fields: [
+    defineField({
+      name: 'title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+      },
+    }),
+    defineField({
+      name: 'author',
+      type: 'reference',
+      to: {type: 'author'},
+    }),
+    defineField({
+      name: 'mainImage',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative text',
+        }
+      ]
+    }),
+    defineField({
+      name: 'categories',
+      type: 'array',
+      of: [defineArrayMember({type: 'reference', to: {type: 'category'}})],
+    }),
+    defineField({
+      name: 'publishedAt',
+      type: 'datetime',
+    }),
+    defineField({
+        name: 'genre',
+        type: 'string',
+        options: {
+            list: [
+                {title: 'Jazz', value: 'jazz'},
+                {title: 'Post Rock', value: 'post-rock'},
+                {title: 'Punk Rock', value: 'punk-rock'},
+                {title: 'Pop Punk', value: 'pop-punk'},
+                {title: 'Alternative Rock', value: 'alternative-rock'},
+                {title: 'Classic Rock', value: 'classic-rock'},
+                {title: 'Hip Hop', value: 'hip-hop'},
+                {title: 'Electronic', value: 'electronic'},
+            ]
+        }
+    }),
+    defineField({
+      name: 'body',
+      type: 'blockContent',
+    }),
+  ],
+  preview: {
+    select: {
+      title: 'title',
+      author: 'author.name',
+      media: 'mainImage',
+    },
+    prepare(selection) {
+      const {author} = selection
+      return {...selection, subtitle: author && `by ${author}`}
+    },
+  },
+})
